8063aa4bdaaf90b90890e69e6581d67651825ab3
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-18 15:30:57 +0900

    recipesテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_18_061507) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_18_061654) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,6 +39,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_061507) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "recipes", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "cooking_time_id", null: false
+    t.text "food", null: false
+    t.text "cooking_method", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -54,4 +65,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_061507) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "recipes", "users"
 end
