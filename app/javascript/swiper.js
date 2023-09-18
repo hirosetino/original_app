bb235ad76e68e0da291a5f5d4b634d05b5869225
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-18 11:07:17 +0900

    トップページ

--- app/javascript/swiper.js
+++ app/javascript/swiper.js
@@ -3,7 +3,7 @@ window.addEventListener("DOMContentLoaded", () => {
         loop: true,
         loopedSlides: 2,
         slidesPerView: "auto",
-        speed: 6000,
+        speed: 8000,
         autoplay: {
             delay: 0,
             disableOnInteraction: false,
