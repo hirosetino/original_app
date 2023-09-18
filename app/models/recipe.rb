beb8f5ff70a9be5a1dc41ef0ce520ff88a80ede5
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-18 15:36:24 +0900

    バリデーション

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,10 @@ class Recipe < ApplicationRecord
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :user
     belongs_to :cooking_time
+
+    has_one_attached :image
+
+    validates :name, presence: true
+    validates :cooking_time_id, presence: true, numericality: { other_than: 1 }
+    validates :food, presence: true
 end
