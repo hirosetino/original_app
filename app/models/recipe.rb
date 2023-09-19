5c9b745ba68e17aa1e493dce9c030fee9b7b7f89
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-19 14:18:48 +0900

    レシピ保存

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,7 +5,9 @@ class Recipe < ApplicationRecord
 
     has_one_attached :image
 
+    validates :image, presence: true
     validates :name, presence: true
     validates :cooking_time_id, presence: true, numericality: { other_than: 1 }
     validates :food, presence: true
+    validates :cooking_method, presence: true
 end
